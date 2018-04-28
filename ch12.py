4a780736397f27153dcd45c6532a773bc89940a0
zhsama <939020488@qq.com>
2018-04-28 19:51:05 +0800

    18/04/27

---
+++ ch12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/27 23:35
+# @Author  : Aries
+# @Site    : 
+# @File    : ch12.py
+# @Software: PyCharm
+
+################        pylint pyflakes pep8        #########################
+a = 1
+b = 2
+c = 3
+print(a)
+print(b)
+print(c)
--- ch9_flask.py
+++ ch9_flask.py
@@ -9,13 +9,13 @@
 
 
 
-from flask import  Flask,render_template
-
-app = Flask(__name__)
-
-@app.route('/echo/thing')
-def echo(thing):
-    return render_template('index.html',thing=thing)
-
-app.run(port=9999,debug=True)
+# from flask import  Flask,render_template
+#
+# app = Flask(__name__)
+#
+# @app.route('/echo/thing')
+# def echo(thing):
+#     return render_template('index.html',thing=thing)
+#
+# app.run(port=9999,debug=True)
 
